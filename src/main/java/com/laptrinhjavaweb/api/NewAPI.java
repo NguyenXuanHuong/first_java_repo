0760f3d50c5bc7f75ef569dab636a4f8540b31c2
NguyenXuanHuong <65778257+NguyenXuanHuong@users.noreply.github.com>
2021-08-16 23:03:52 +0700

    Update NewAPI.java

--- src/main/java/com/laptrinhjavaweb/api/NewAPI.java
+++ src/main/java/com/laptrinhjavaweb/api/NewAPI.java
@@ -1,5 +1,6 @@
 package com.laptrinhjavaweb.api;
 // huong changed
+//huong changed2
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
